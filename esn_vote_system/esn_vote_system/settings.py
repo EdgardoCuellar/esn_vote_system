31a2f47b2838c310d823b89a46a4fc4c42511b0d
Edgardo Cuellar Sanchez <45764390+EdgardoCuellar@users.noreply.github.com>
2024-05-05 22:16:14 +0200

    put static root

--- esn_vote_system/esn_vote_system/settings.py
+++ esn_vote_system/esn_vote_system/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import os.path  
+import sys
+
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
